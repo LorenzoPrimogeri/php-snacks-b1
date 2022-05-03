a5acbe3124f9560c5998c3be6f874c98776464fd
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-05-03 16:54:09 +0200

    added count on for

--- snack_Bonus/index.php
+++ snack_Bonus/index.php
@@ -4,18 +4,30 @@ Creare un array contenente qualche alunno di un’ipotetica classe.
 Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici.
 Stampare Nome, Cognome e la media dei voti di ogni alunno.
 */
-$arrayAlunni = [
-    "nome" => "lorenzo",
-    "cognnome" => "primogeri",
-    "voti" => [1, 10, 5, 4, 32]
-];
-for ($i = 0; $i < $arrayAlunni; $i++) {
-    echo ("nome:" . $arrayAlunni["nome"]);
-    echo ("cognnome:" . $arrayAlunni["cognnome"]);
-    for ($j = 0; $j < $arrayAlunni["voti"]; $j++) {
-        echo ("sono al interno dei voti");
+// function calcolaMedia($nums)
+// {
+//     $qt = count($nums);
+//     $res = 0;
+//     foreach ($nums as $n) {
+//         $res += $n;
+//     }
+//     return $res / $qt;
+// }
+$arrayAlunni = array(
+    array("Lorenzo Primogeri", [4, 5, 6, 7]),
+    array("Giovanni Primogeri", [6, 5, 7, 8]),
+    array("Saab astiano", [2, 3, 4, 5]),
+    array("Land Rover", [5, 6, 7, 8])
+);
+
+for ($i = 0; $i < count($arrayAlunni); $i++) {
+    var_dump($arrayAlunni[$i]);
+    for ($j = 0; $j < count($arrayAlunni); $j++) {
+        $element = $arrayAlunni[$i][$j];
+        echo ($element);
     }
 }
+
 
 
 
