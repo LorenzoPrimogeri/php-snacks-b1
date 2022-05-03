0b3d1c844db5a346ada5f510068a5bc98ad35bb9
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-05-03 16:16:42 +0200

    inifinity loop problem

--- snack_Bonus/index.php
+++ snack_Bonus/index.php
@@ -4,21 +4,16 @@ Creare un array contenente qualche alunno di un’ipotetica classe.
 Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici.
 Stampare Nome, Cognome e la media dei voti di ogni alunno.
 */
-// $arrayAlunni = [
-//     "nome" => "lorenzo",
-//     "cognnome" => "primogeri",
-//     "voti" => [1, 10, 5, 4, 32]
-// ];
-$arrayAlunni = array(
-    array("Lorenzo Primogeri", [22, 18, 22, 18, 22, 18]),
-    array("Paolo Botrugno", 15, 1322, 18, 22, 18, 22),
-    array("Paolo carso", 5, 2, 22, 18, 22, 18, 22),
-    array("Land Rover", 17, 15, 22, 18, 22, 18, 22)
-);
+$arrayAlunni = [
+    "nome" => "lorenzo",
+    "cognnome" => "primogeri",
+    "voti" => [1, 10, 5, 4, 32]
+];
 for ($i = 0; $i < $arrayAlunni; $i++) {
-    echo ("nome:" . $arrayAlunni[$i]);
+    echo ("nome:" . $arrayAlunni["nome"]);
+    echo ("cognnome:" . $arrayAlunni["cognnome"]);
     for ($j = 0; $j < $arrayAlunni["voti"]; $j++) {
-        echo ("sono al intenrso");
+        echo ("sono al interno dei voti");
     }
 }
 
